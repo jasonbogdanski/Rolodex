bfa29bf046e7ce293d2ca7684c77287424ab1d2c
Jason Bogdanski <jason.bogdanski@gmail.com>
2022-09-11 17:14:56 -0400

    Added Rolodex.Messages project for Message Bus Messages.

---
+++ Rolodex.Messages/SendEmail.cs
@@ -0,0 +1,11 @@
+﻿using NServiceBus;
+
+namespace Rolodex.Messages
+{
+    public record SendEmail : IEvent
+    {
+        public string ToEmailAddress { get; init; } = null!;
+        public string Subject { get; init; } = null!;
+        public string Body { get; init; } = null!;
+    }
+}
