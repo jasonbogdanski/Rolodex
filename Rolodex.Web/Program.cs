9ebf8f91d19fb9c9d33ef7bd8ec1d6b6d8549e0a
Jason Bogdanski <jason.bogdanski@gmail.com>
2022-09-11 17:34:02 -0400

    Added NServiceBus configuration to Rolodex.Web.

--- Rolodex.Web/Program.cs
+++ Rolodex.Web/Program.cs
@@ -6,66 +6,95 @@ using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Identity.Web;
 using Microsoft.Identity.Web.UI;
+using NServiceBus;
 using Rolodex.Web.DataStore;
 using Rolodex.Web.Infrastructure;
 using Rolodex.Web.Infrastructure.Tags;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
-    .AddMicrosoftIdentityWebApp(builder.Configuration.GetSection("AzureAd"));
-
-builder.Services.AddAuthorization(options =>
-{
-    // By default, all incoming requests will be authorized according to the default policy.
-    options.FallbackPolicy = options.DefaultPolicy;
-});
-
-builder.Services.AddDbContext<RolodexContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
-builder.Services.AddAutoMapper(typeof(Program));
-
-builder.Services.AddMediatR(typeof(Program));
-
-builder.Services.AddHtmlTags(new TagConventions());
-
-builder.Services.AddRazorPages(opt =>
-    {
-        opt.Conventions.ConfigureFilter(new DbContextTransactionPageFilter());
-        opt.Conventions.ConfigureFilter(new ValidatorPageFilter());
-    })
-    .AddMicrosoftIdentityUI();
-
-builder.Services.AddMvc(opt => opt.ModelBinderProviders.Insert(0, new EntityModelBinderProvider()));
-
-builder.Services.AddFluentValidationAutoValidation(config =>
-{
-    config.DisableDataAnnotationsValidation = true;
-});
-
-builder.Services.AddValidatorsFromAssemblyContaining<Program>();
+RegisterServices(builder);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthentication();
-app.UseAuthorization();
-
-app.MapRazorPages();
-app.MapControllers();
+ConfigureApplication(app);
 
 app.Run();
+
+void RegisterServices(WebApplicationBuilder webApplicationBuilder)
+{
+    webApplicationBuilder.Host.UseNServiceBus(context =>
+    {
+        var endpointConfiguration = new EndpointConfiguration("Rolodex.Web");
+
+        endpointConfiguration.UseTransport<LearningTransport>();
+
+        endpointConfiguration.SendFailedMessagesTo("error");
+        endpointConfiguration.AuditProcessedMessagesTo("audit");
+        endpointConfiguration.SendHeartbeatTo("Particular.ServiceControl");
+
+        var metrics = endpointConfiguration.EnableMetrics();
+        metrics.SendMetricDataToServiceControl("Particular.Monitoring", TimeSpan.FromMilliseconds(500));
+
+        return endpointConfiguration;
+    });
+
+// Add services to the container.
+    webApplicationBuilder.Services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
+        .AddMicrosoftIdentityWebApp(webApplicationBuilder.Configuration.GetSection("AzureAd"));
+
+    webApplicationBuilder.Services.AddAuthorization(options =>
+    {
+        // By default, all incoming requests will be authorized according to the default policy.
+        options.FallbackPolicy = options.DefaultPolicy;
+    });
+
+    webApplicationBuilder.Services.AddDbContext<RolodexContext>(options =>
+        options.UseSqlServer(webApplicationBuilder.Configuration.GetConnectionString("DefaultConnection")));
+
+    webApplicationBuilder.Services.AddAutoMapper(typeof(Program));
+
+    webApplicationBuilder.Services.AddMediatR(typeof(Program));
+
+    webApplicationBuilder.Services.AddHtmlTags(new TagConventions());
+
+    webApplicationBuilder.Services.AddRazorPages(opt =>
+        {
+            opt.Conventions.ConfigureFilter(new DbContextTransactionPageFilter());
+            opt.Conventions.ConfigureFilter(new ValidatorPageFilter());
+        })
+        .AddMicrosoftIdentityUI();
+
+    webApplicationBuilder.Services.AddMvc(opt => opt.ModelBinderProviders.Insert(0, new EntityModelBinderProvider()));
+
+    webApplicationBuilder.Services.AddFluentValidationAutoValidation(config =>
+    {
+        config.DisableDataAnnotationsValidation = true;
+    });
+
+    webApplicationBuilder.Services.AddValidatorsFromAssemblyContaining<Program>();
+}
+
+void ConfigureApplication(WebApplication webApplication)
+{
+// Configure the HTTP request pipeline.
+    if (!webApplication.Environment.IsDevelopment())
+    {
+        webApplication.UseExceptionHandler("/Error");
+        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+        webApplication.UseHsts();
+    }
+
+    webApplication.UseHttpsRedirection();
+    webApplication.UseStaticFiles();
+
+    webApplication.UseRouting();
+
+    webApplication.UseAuthentication();
+    webApplication.UseAuthorization();
+
+    webApplication.MapRazorPages();
+    webApplication.MapControllers();
+}
+
+
