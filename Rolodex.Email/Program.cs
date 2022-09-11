805abdc267cdab58101c1fdb3c08344cfd84d09a
Jason Bogdanski <jason.bogdanski@gmail.com>
2022-09-11 14:47:47 -0400

    Added Rolodex.Email service to send confirmation emails.
    Added basic NServiceBus setup to console application.

---
+++ Rolodex.Email/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Microsoft.Extensions.Hosting;
+using NServiceBus;
+
+var host = Host.CreateDefaultBuilder(args)
+    .UseNServiceBus(context =>
+    {
+        var endpointConfiguration = new EndpointConfiguration("RolodexEmail");
+
+        endpointConfiguration.UseTransport<LearningTransport>();
+
+        endpointConfiguration.SendFailedMessagesTo("error");
+        endpointConfiguration.AuditProcessedMessagesTo("audit");
+        endpointConfiguration.SendHeartbeatTo("Particular.ServiceControl");
+
+        var metrics = endpointConfiguration.EnableMetrics();
+        metrics.SendMetricDataToServiceControl("Particular.Monitoring", TimeSpan.FromMilliseconds(500));
+
+        return endpointConfiguration;
+    })
+    .Build();
+
+host.Run();
