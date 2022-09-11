5c22b0dcf8cc653dfd471f510b3931aaa0d97f8b
Jason Bogdanski <jason.bogdanski@gmail.com>
2022-09-11 13:11:36 -0400

    Added index integration test for employees.

---
+++ Rolodex.IntegrationTests/Pages/Employee/IndexTests.cs
@@ -0,0 +1,58 @@
+﻿using FluentAssertions;
+using Rolodex.Models;
+using Rolodex.Pages.Employee;
+using Index = Rolodex.Pages.Employee.Index;
+
+namespace Rolodex.IntegrationTests.Pages.Employee;
+
+[Collection(nameof(TestingFixture))]
+public class IndexTests
+{
+    private readonly TestingFixture _fixture;
+
+    public IndexTests(TestingFixture fixture) => _fixture = fixture;
+
+    [Fact]
+    public async Task Should_list_employees()
+    {
+        var branch = new CompanyBranch
+        {
+            Name = "Test Branch",
+            City = "Akron",
+            State = "Ohio"
+        };
+
+        await _fixture.InsertAsync(branch);
+
+        var command = new CreateEdit.Command
+        {
+            FirstName = "George",
+            LastName = "Smith",
+            JobTitle = "Accountant",
+            Email = "test@test.com",
+            CompanyBranch = branch
+        };
+
+        var employeeId1 = await _fixture.SendAsync(command);
+
+        command = new CreateEdit.Command
+        {
+            FirstName = "John",
+            LastName = "Miller",
+            JobTitle = "QA",
+            Email = "qa@test.com",
+            CompanyBranch = branch
+        };
+
+        var employeeId2 = await _fixture.SendAsync(command);
+
+        var query = new Index.Query();
+
+        var result = await _fixture.SendAsync(query);
+
+        result.Should().NotBeNull();
+        result.Employees.Count.Should().BeGreaterOrEqualTo(2);
+        result.Employees.Select(m => m.Id).Should().Contain(employeeId1);
+        result.Employees.Select(m => m.Id).Should().Contain(employeeId2);
+    }
+}
