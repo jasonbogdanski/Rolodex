4d637d35d14936531a97ca1f05a569c13c795585
Jason Bogdanski <jason.bogdanski@gmail.com>
2022-09-11 17:16:51 -0400

    Added stubbed sendemail handler.

---
+++ Rolodex.Email/SendEmailHandler.cs
@@ -0,0 +1,13 @@
+﻿using NServiceBus;
+using Rolodex.Messages;
+
+namespace Rolodex.Email
+{
+    public class SendEmailHandler : IHandleMessages<SendEmail>
+    {
+        public Task Handle(SendEmail message, IMessageHandlerContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
