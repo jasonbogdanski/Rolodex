9fc2cba95e632b2d9c1c45e52a1d20d3475aed3d
Jason Bogdanski <jason.bogdanski@gmail.com>
2022-09-11 22:54:58 -0400

    Added Azure Email Communication Service to send email.
    Used secrets manager to keep connection strings local.

--- Rolodex.Email/Program.cs
+++ Rolodex.Email/Program.cs
@@ -1,10 +1,33 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using NServiceBus;
 
-var host = Host.CreateDefaultBuilder(args)
-    .UseNServiceBus(context =>
+var host = Host.CreateDefaultBuilder(args);
+
+RegisterServices(host);
+
+var built = host.Build();
+
+built.Run();
+
+void RegisterServices(IHostBuilder hostBuilder)
+{
+    hostBuilder.ConfigureServices((context, services)  =>
+    {
+        var configuration = context.Configuration;
+        services.AddOptions<EmailCommunicationOptions>()
+            .Bind(configuration.GetSection("EmailCommunication"));
+    });
+
+    hostBuilder.ConfigureAppConfiguration(config =>
+    {
+        config.AddUserSecrets<Program>();
+    });
+
+    hostBuilder.UseNServiceBus(context =>
     {
         var endpointConfiguration = new EndpointConfiguration("Rolodex.Email");
 
@@ -18,7 +41,11 @@ var host = Host.CreateDefaultBuilder(args)
         metrics.SendMetricDataToServiceControl("Particular.Monitoring", TimeSpan.FromMilliseconds(500));
 
         return endpointConfiguration;
-    })
-    .Build();
+    });
+}
 
-host.Run();
+public record EmailCommunicationOptions
+{
+    public string ConnectionString { get; set; } = null!;
+    public string ReplyToEmail { get; set; } = null!;
+}
--- Rolodex.Email/SendEmailHandler.cs
+++ Rolodex.Email/SendEmailHandler.cs
@@ -1,13 +1,38 @@
-﻿using NServiceBus;
+﻿using Azure.Communication.Email;
+using Azure.Communication.Email.Models;
+using Microsoft.Extensions.Options;
+using NServiceBus;
 using Rolodex.Messages;
 
 namespace Rolodex.Email
 {
     public class SendEmailHandler : IHandleMessages<SendEmail>
     {
-        public Task Handle(SendEmail message, IMessageHandlerContext context)
+        private readonly EmailCommunicationOptions _emailCommunicationOptions;
+
+        public SendEmailHandler(IOptions<EmailCommunicationOptions> connectionStringOptions)
         {
-            throw new NotImplementedException();
+            _emailCommunicationOptions = connectionStringOptions.Value;
+        }
+
+        public async Task Handle(SendEmail message, IMessageHandlerContext context)
+        {
+            var emailClient =
+                new EmailClient(_emailCommunicationOptions.ConnectionString);
+
+            var emailContent = new EmailContent(message.Subject)
+            {
+                Html = message.Body
+            };
+
+            var emailAddress = new List<EmailAddress> { new(message.ToEmailAddress) };
+            var emailRecipients = new EmailRecipients(emailAddress);
+            var emailMessage =
+                new EmailMessage(_emailCommunicationOptions.ReplyToEmail
+                    , emailContent
+                    , emailRecipients);
+
+            await emailClient.SendAsync(emailMessage);
         }
     }
 }
